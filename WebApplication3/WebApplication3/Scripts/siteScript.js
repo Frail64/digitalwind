1779891d0095b1c513ea7737314866b796ee1e5d
Vladislav Kudashev <vladislav_kudashev@epam.com>
2015-03-11 16:05:17 +0300

    init

---
+++ WebApplication3/WebApplication3/Scripts/siteScript.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    var $popupbg = $('.popup-bg'),
+        $popup = $('.popup');
+
+    $('.thumbnail').on('click', function () {
+        var $this = $(this);
+        $popup.html($this.html());
+        $popupbg.show(500);
+        debugger;
+    });
+
+    $popupbg.on('click', function(){
+        $popupbg.hide(500);
+    });
+
+}());
